0d3d42b207caab8d3f960dd718fd2034f88f858c
maycon silva <maycon8609@gmail.com>
2024-02-10 16:21:49 -0300

    feat: add validator to create tag body

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -5,6 +5,8 @@ from src.views.tag_creator_view import TagCreatorView
 
 from src.errors.error_handler import error_handler
 
+from src.validators.tag_creator_validator import tag_creator_validator
+
 tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=["POST"])
@@ -12,6 +14,7 @@ def create_tag():
     response = None
 
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
 
         http_request = HttpRequest(body=request.json)
